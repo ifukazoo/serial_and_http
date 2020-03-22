0b0694d088abef2666b4655092bdfa2ebaf80bfe
ifukazoo <ifukazoo@gmail.com>
2020-03-22 18:38:32 +0900

    サブルーチン化

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,6 +5,11 @@ use std::io;
 use std::io::Write;
 use std::process;
 
+enum Cmd {
+    Quit,
+    Astro,
+}
+
 fn main() {
     let args: Vec<OsString> = env::args_os().collect();
 
@@ -18,33 +23,45 @@ fn main() {
     println!("port {:?} opened.", port_name);
 
     loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let cmd = input.to_uppercase();
-        if cmd.starts_with("Q") {
-            break;
-        }
-        let mut data: Vec<u8> = vec![0x2];
-        data.append(&mut String::from("http://api.open-notify.org/astros.json").into_bytes());
-        data.push(0x3);
-        let mut slice = &data[..];
-        // 送信
-        loop {
-            match port.write(slice) {
-                Ok(size) => {
-                    if size == slice.len() {
-                        break;
-                    }
-                    slice = &data[size..];
-                }
-                Err(e) => {
-                    eprintln!("write serial port fail.[{}]", e);
-                    process::exit(1);
-                }
+        match get_cmd() {
+            Cmd::Quit => break,
+            _ => {
+                let url = String::from("http://api.open-notify.org/astros.json");
+                write_serial(&mut port, &url).unwrap();
             }
         }
-        // 受信
     }
     println!("client exit");
     process::exit(0);
+}
+
+fn write_serial(port: &mut Box<dyn serialport::SerialPort>, s: &str) -> Result<(), io::Error> {
+    let mut data: Vec<u8> = vec![0x2];
+    data.append(&mut String::from(s).into_bytes());
+    data.push(0x3);
+    let mut slice = &data[..];
+    // 送信
+    loop {
+        match port.write(slice) {
+            Ok(size) => {
+                if size == slice.len() {
+                    break;
+                }
+                slice = &data[size..];
+            }
+            Err(e) => return Err(e),
+        }
+    }
+    Ok(())
+}
+
+fn get_cmd() -> Cmd {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let cmd = input.to_uppercase();
+    if cmd.starts_with("Q") {
+        Cmd::Quit
+    } else {
+        Cmd::Astro
+    }
 }
