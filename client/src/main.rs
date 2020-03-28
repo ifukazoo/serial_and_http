de5c962f27a60f9b62e55eb82f157e7a77a581c8
ifukazoo <ifukazoo@gmail.com>
2020-03-29 07:37:11 +0900

    最初の動作完成

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,5 @@
 extern crate mylib;
+
 use std::env;
 use std::ffi::OsString;
 use std::io;
@@ -7,38 +8,64 @@ use std::process;
 enum Cmd {
     Quit,
     Astro,
+    Coin,
+    None,
 }
 
 static ASTRO_URL: &str = "http://api.open-notify.org/astros.json";
+static COIN_URL: &str = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 fn main() {
     let args: Vec<OsString> = env::args_os().collect();
 
     if args.len() < 2 {
-        eprintln!("<cmd> <serial port name>");
+        eprintln!("client> <cmd> <serial port name>");
         process::exit(1);
     }
 
     let port_name = &args[1];
     let mut port = mylib::open_serialport(port_name).unwrap_or_else(|e| {
         let err = format!("could not open port {:?}. reason [{}]", port_name, e);
-        eprintln!("{}", err);
+        eprintln!("client> {}", err);
         process::exit(1);
     });
 
     loop {
-        match get_cmd() {
+        // 端末入力を待つ
+        println!("client> waiting cmd ...");
+        let cmd = get_cmd();
+
+        match cmd {
             Cmd::Quit => break,
-            Cmd::Astro => {
-                let url = String::from(ASTRO_URL);
+
+            Cmd::Astro | Cmd::Coin => {
+                let url = match cmd {
+                    Cmd::Astro => String::from(ASTRO_URL),
+                    Cmd::Coin => String::from(COIN_URL),
+                    _ => panic!("must not reach"),
+                };
+
+                // サーバー側にURLを送信
+                println!("client> request to server.");
                 mylib::write_to(&mut port, &url).unwrap();
+
+                // レスポンス待ち
                 let s = mylib::read_from(&mut port).unwrap();
-                println!("{}", s);
+                println!("client> response from server. [{}]", s);
             }
+            Cmd::None => usage(),
         }
     }
-    println!("exit process.");
+    println!("client> exit process.");
     process::exit(0);
+}
+
+fn usage() {
+    println!("client>");
+    println!("command not found.");
+    println!(r#"  "astro""#);
+    println!(r#"  "coin""#);
+    println!("are available.");
 }
 
 fn get_cmd() -> Cmd {
@@ -47,7 +74,11 @@ fn get_cmd() -> Cmd {
     let cmd = input.to_uppercase();
     if cmd.starts_with("Q") {
         Cmd::Quit
-    } else {
+    } else if cmd.starts_with("ASTRO") {
         Cmd::Astro
+    } else if cmd.starts_with("COIN") {
+        Cmd::Coin
+    } else {
+        Cmd::None
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,6 @@
 extern crate mylib;
+extern crate reqwest;
+
 use std::env;
 use std::ffi::OsString;
 use std::process;
@@ -7,19 +9,45 @@ fn main() {
     let args: Vec<OsString> = env::args_os().collect();
 
     if args.len() < 2 {
-        eprintln!("<cmd> <serial port name>");
+        eprintln!("server> <cmd> <serial port name>");
         process::exit(1);
     }
 
     let port_name = &args[1];
     let mut port = mylib::open_serialport(port_name).unwrap_or_else(|e| {
         let err = format!("could not open port {:?}. reason [{}]", port_name, e);
-        eprintln!("{}", err);
+        eprintln!("server> {}", err);
         process::exit(1);
     });
     loop {
+        println!("server> waiting from client ...");
+
+        // クライアントからのURLを待つ
         let url = mylib::read_from(&mut port).unwrap();
-        println!("data has come. [{}]", url);
-        mylib::write_to(&mut port, &url).unwrap();
+        println!("server> data has come. [{}]", url);
+
+        // HTTP Request
+        let res = match request(&url) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!("server> error happened.");
+                e
+            }
+        };
+        println!("server> response to.");
+        mylib::write_to(&mut port, &res).unwrap();
+    }
+}
+
+fn request(url: &str) -> Result<String, String> {
+    let result = reqwest::blocking::get(url);
+    if let Err(result) = result {
+        Err(result.to_string())
+    } else {
+        let text = result.unwrap().text();
+        match text {
+            Ok(text) => Ok(text),
+            Err(e) => Err(e.to_string()),
+        }
     }
 }
